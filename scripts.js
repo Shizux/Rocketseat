c62023cd6daf45bc3d1fa7724875359667f3044d
Shizux <54588680+Shizux@users.noreply.github.com>
2023-03-22 11:48:24 -0300

    Resultaod

--- scripts.js
+++ scripts.js
@@ -7,6 +7,35 @@ despesas e irá mostrar uma mensagem se a família está com
 saldo positivo ou negativo, seguido do valor do saldo.
 */
 
-const receitas = [
-    1000, 2000, 3000
-]
+let familia = {                  /*  Objeto criado    */
+    receitas: [1000, 2000],
+    despesas: [1000, 3000]
+}
+
+function soma(array) {    /* função para calcular os valores dentro do array */
+    let total = 0;
+
+    for(let valor of array){
+        total += valor
+    }
+        return total
+    }
+
+
+function balanco() {    /* função para calcular e mostrar o resultado */
+    const calcularReceitas = soma(familia.receitas)
+    const calcularDespesas = soma(familia.despesas)
+
+    const total = calcularReceitas - calcularDespesas
+    
+    const ok = total >= 0 
+
+    let texto = "Negativo"
+
+    if (ok) {
+        texto = "Positivo"
+    }
+    console.log (`Seu saldo é ${texto}: ${total}`)        
+}
+
+balanco()
