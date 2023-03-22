1732b59fe154c29e07835f9b0f9b5c46c1391e29
Shizux <54588680+Shizux@users.noreply.github.com>
2023-03-22 11:03:52 -0300

    Teste

---
+++ scripts.js
@@ -0,0 +1,12 @@
+/*
+Crie um objeto que possuirá 2 propriedades, ambas do tipo array:
+    * receitas: [] 
+    * despesas: []
+Agora, crie uma função que irá calcular o total de receitas e 
+despesas e irá mostrar uma mensagem se a família está com 
+saldo positivo ou negativo, seguido do valor do saldo.
+*/
+
+const receitas = [
+    1000, 2000, 3000
+]
